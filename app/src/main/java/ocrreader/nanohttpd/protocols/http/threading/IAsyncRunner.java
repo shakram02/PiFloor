e97c0844d4569542d6142237eb045223d5e55bd9
shakram02 <ahmedhamdyau@gmail.com>
2018-05-01 15:28:01 +0200

    include nanohttpd sources, provided jar isn't updated

---
+++ app/src/main/java/ocrreader/nanohttpd/protocols/http/threading/IAsyncRunner.java
@@ -0,0 +1,25 @@
+package ocrreader.nanohttpd.protocols.http.threading;
+
+/*
+ * #%L
+ * NanoHttpd-Core
+ * %%
+ * Copyright (C) 2012 - 2016 nanohttpd
+ * %%
+
+ * #L%
+ */
+
+import ocrreader.nanohttpd.protocols.http.ClientHandler;
+
+/**
+ * Pluggable strategy for asynchronously executing requests.
+ */
+public interface IAsyncRunner {
+
+    void closeAll();
+
+    void closed(ClientHandler clientHandler);
+
+    void exec(ClientHandler code);
+}
