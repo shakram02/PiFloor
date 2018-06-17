1dc604fc54c692408dc8f7d4340fc9ecba9e8640
shakram02 <ahmedhamdyau@gmail.com>
2018-06-17 17:09:36 +0200

    add contains function

--- app/src/main/java/ocrreader/GridItemHolder.kt
+++ app/src/main/java/ocrreader/GridItemHolder.kt
@@ -6,11 +6,14 @@ class GridItemHolder(private val application: Application) {
     private val entries = HashSet<String>()
     val size
         get() = entries.size
-
     val items
         get() = entries.joinToString()
 
     fun addGridItem(item: String): Boolean {
         return entries.add(item.toLowerCase())
+    }
+
+    fun contains(item: String): Boolean {
+        return entries.contains(item)
     }
 }
