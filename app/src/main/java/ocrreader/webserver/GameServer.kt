057d7403b57b65776af395ba7357b3b6566c6298
shakram02 <ahmedhamdyau@gmail.com>
2018-06-19 18:37:23 +0200

    refactor address computation methods

--- app/src/main/java/ocrreader/webserver/GameServer.kt
+++ app/src/main/java/ocrreader/webserver/GameServer.kt
@@ -7,19 +7,28 @@ import java.io.IOException
 
 class GameServer(private val hostName: String, private val listenPort: Int, private val topicName: String) {
     var server = AsyncHttpServer()
-    val address: String
-        get() = hostName
-    val port: Int
-        get() = listenPort
+    val webAddress: String
+        get() = computeWebAddress()
+    val webSocketAddress: String
+        get() = "ws://$hostName:$listenPort/$topicName"
 
     @Throws(IOException::class)
     fun start() {
         server.websocket("/$topicName", WebSocketGameServer())
-        server.get("/.*", HttpGameServer())
-
+        server.get("/.*", HttpGameServer()) // Match all routes
         server.listen(listenPort)
-        Log.i(TAG, "WebSocket: ws://$hostName:$listenPort/$topicName")
+
+        Log.i(TAG, "HTTP: $webAddress")
+        Log.i(TAG, "WebSocket: $webSocketAddress")
     }
+
+    private fun computeWebAddress(): String {
+        return when (listenPort) {
+            DEFAULT_HTTP_PORT -> "http://$hostName"
+            else -> "http://$hostName:$listenPort"
+        }
+    }
+
 
     fun stop() {
         server.stop()
@@ -27,5 +36,6 @@ class GameServer(private val hostName: String, private val listenPort: Int, priv
 
     companion object {
         private const val TAG = "WebServer"
+        private const val DEFAULT_HTTP_PORT: Int = 80
     }
 }
