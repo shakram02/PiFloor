b80508c040cb2b432330ed77eed4270063de8920
shakram02 <ahmedhamdyau@gmail.com>
2018-06-22 18:44:40 +0200

    complete http server implementation

--- app/src/main/java/ocrreader/webserver/HttpGameServer.kt
+++ app/src/main/java/ocrreader/webserver/HttpGameServer.kt
@@ -9,28 +9,48 @@ import java.io.InputStreamReader
 
 class HttpGameServer(private val context: Application) : HttpServerRequestCallback {
     override fun onRequest(request: AsyncHttpServerRequest, response: AsyncHttpServerResponse) {
-        Log.i(TAG, "HTTP:${request.method}, ${request.path}")
-        // TODO: parse the path and load the requested resource
-        if (request.path == "/") {
-            try {
-                val assetManager = context.assets
+        val assetManager = context.assets
+        val requestedFile: String = sanitizeRequestedPath(request.path)
+        Log.i(TAG, "HTTP:${request.method}, $requestedFile")
 
-                // TODO move file operation to background thread
-                val reader = InputStreamReader(assetManager.open("index.html"))
-                val page = reader.readText()
-                Log.i(TAG, "Page Result:$page")
-                response.send(page)
-
-            } catch (e: Exception) {
-                Log.e(TAG, "Failed to load web page", e)
+        try {
+            if (requestedFile.matches(Regex(EMPTY_FILE_NAME_REGEX))) {
+                response.end()
+                return
             }
-            return
-        }
 
-        response.send("Hello, nothing is here")
+            if (!assetManager.list("").contains(requestedFile)) {
+                response.end()
+                return
+            }
+
+            val reader = InputStreamReader(assetManager.open(requestedFile))
+            val asset = reader.readText()
+
+            response.send(asset)
+        } catch (e: Exception) {
+            Log.e(TAG, "Failed to load web page", e)
+        }
+    }
+
+    private fun sanitizeRequestedPath(path: String): String {
+        val withoutSlash = path.replace("/", "")
+        return if (withoutSlash.matches(Regex(FILE_NAME_WITH_EXTENSION_REGEX))) {
+            withoutSlash
+        } else {
+            if (withoutSlash.isEmpty()) {
+                // Root was requested
+                INDEX_HTML_FILE_NAME
+            } else {
+                "$withoutSlash.html"
+            }
+        }
     }
 
     companion object {
         private const val TAG = "HttpGameServer"
+        private const val EMPTY_FILE_NAME_REGEX = "\\..*"
+        private const val FILE_NAME_WITH_EXTENSION_REGEX = ".*\\..*"
+        private const val INDEX_HTML_FILE_NAME = "index.html"
     }
 }
