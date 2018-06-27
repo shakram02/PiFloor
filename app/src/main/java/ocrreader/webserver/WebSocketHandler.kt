6e63aa597fda2e75431d6b595d5bbcbfbee3acda
shakram02 <ahmedhamdyau@gmail.com>
2018-06-27 17:28:44 +0200

    store a handle to client, allow a single client

--- app/src/main/java/ocrreader/webserver/WebSocketHandler.kt
+++ app/src/main/java/ocrreader/webserver/WebSocketHandler.kt
@@ -7,29 +7,42 @@ import com.koushikdutta.async.http.server.AsyncHttpServer
 import com.koushikdutta.async.http.server.AsyncHttpServerRequest
 
 class WebSocketHandler : AsyncHttpServer.WebSocketRequestCallback {
-    private val sockets = ArrayList<WebSocket>()
+    private var client: WebSocket? = null
+
+    private val onCloseCallback = CompletedCallback { ex ->
+        try {
+            if (ex != null)
+                Log.e("WebSocket", "Error")
+        } finally {
+            client = null
+        }
+    }
 
     override fun onConnected(webSocket: WebSocket, request: AsyncHttpServerRequest?) {
-        sockets.add(webSocket)
-        //Use this to clean up any references to your webSocket
-        webSocket.closedCallback = CompletedCallback { ex ->
-            try {
-                if (ex != null)
-                    Log.e("WebSocket", "Error")
-            } finally {
-                sockets.remove(webSocket)
-            }
+        if (client != null) {
+            Log.wtf(TAG, "Another client tried to connect to WebSocket server")
+            webSocket.close()
         }
+
+        client = webSocket
+        Log.d(TAG, "A client connected")
+
+        //Use this to clean up any references to your webSocket
+        webSocket.closedCallback = onCloseCallback
+
         webSocket.stringCallback = WebSocket.StringCallback { string ->
             Log.i(TAG, "Client sent:$string")
             webSocket.send("Welcome Client!")
         }
+
+        webSocket.endCallback = onCloseCallback
     }
 
     fun broadcast(message: String) {
-        for (socket in sockets) {
-            socket.send(message)
-        }
+        if (client == null) return
+
+        client?.send(message)
+        Log.d(TAG, "Sending:$message")
     }
 
     companion object {
