4b63ece2c7db99058a3601f346eea9b1735c962c
Ahmed Hamdy (@shakam02) <ahmedhamdyau@gmail.com>
2018-09-16 13:56:39 +0200

    Store label point (#19)
    * remove unwanted library because function limit exceeded
    
    * encode position with grid text, encapsulate text preprocessing into GridHolder
    
    * return diff result as labeled points
    
    * fix diffing

--- app/src/main/java/ocrreader/utils/GridItemHolder.kt
+++ app/src/main/java/ocrreader/utils/GridItemHolder.kt
@@ -1,34 +1,85 @@
 package ocrreader.utils
 
-import com.google.android.gms.vision.text.TextBlock
+import android.graphics.Point
+import android.graphics.Rect
+import android.util.Log
+import com.google.android.gms.vision.text.Text
 
 class GridItemHolder {
-    private val entries = HashSet<String>()
+    private val entries = HashSet<GridText>()
     val size
         get() = entries.size
-    val itemsAsString
-        get() = entries.asIterable()
 
-    fun addGridItem(item: String): Boolean {
-        return entries.add(item.preProcess())
+    val itemsAsString
+        get() = entries.asIterable().map { i -> i.value }
+
+    fun addGridItem(item: Text): Boolean {
+        return entries.add(GridText(item))
     }
 
-    fun contains(item: String): Boolean {
-        return entries.contains(item.preProcess())
+    fun contains(item: Text): Boolean {
+        return entries.any { i -> i.value == preProcess(item.value) }
     }
 
     fun clear() {
         entries.clear()
     }
 
-    fun diff(items: ArrayList<TextBlock>): Set<String> {
-        return entries.subtract(items
+    fun diff(items: List<Text>): Set<Pair<String, Point>> {
+        val externalEntrySet = items
                 .filter { item -> item.value != null }
-                .map { item -> item.value.preProcess() }
-                .toHashSet())
+                .map { item -> GridText(item) }
+                .toHashSet()
+
+        return entries.subtract(externalEntrySet).toLabeledPoints()
     }
 
-    private fun String.preProcess(): String {
-        return this.toLowerCase().replace(" ", "")
+    companion object {
+        private fun preProcess(s: String): String {
+            return s.toLowerCase().replace(" ", "")
+        }
+    }
+
+    private fun Iterable<GridText>.toLabeledPoints(): Set<Pair<String, Point>> {
+        return this.map { label -> Pair(label.value, label.getCenter()) }.toHashSet()
+    }
+
+    /**
+     * Instantiating an instance of a [Text] is not possible, this class
+     * gives us this ability (just a wrapper class)
+     */
+    private data class GridText(private val textItem: Text) : Text {
+        private val textBoundingBox: Rect = textItem.boundingBox
+        private val textComponents: MutableList<out Text> = textItem.components
+        private val textCornerPoints: Array<Point> = textItem.cornerPoints
+        private val textValue: String = GridItemHolder.preProcess(textItem.value)
+
+        override fun getBoundingBox(): Rect {
+            return textBoundingBox
+        }
+
+        override fun getComponents(): MutableList<out Text> {
+            return textComponents
+        }
+
+        override fun getCornerPoints(): Array<Point> {
+            return textCornerPoints
+        }
+
+        override fun getValue(): String {
+            return textValue
+        }
+
+        fun getCenter(): Point {
+            return Point(textBoundingBox.centerX(), textBoundingBox.centerY())
+        }
+
+        override fun hashCode(): Int {
+            return textValue.hashCode()
+        }
+
+        override fun equals(other: Any?): Boolean {
+            return this.hashCode() == other!!.hashCode()
+        }
     }
 }
