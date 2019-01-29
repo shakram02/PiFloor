45ad9b773e31f387a56e12b7ed2194a95bfe49fe
AhmedHammad <hammad97official@gmail.com>
2019-01-29 13:43:11 +0200

    Structure admin page

--- app/src/main/assets/admin.js
+++ app/src/main/assets/admin.js
@@ -1,8 +1,16 @@
-var adminPage = Vue.component('adminPage', {
+var adminPage = Vue.component('admin', {
   data: function () {
     return {
       count: 0
     }
   },
-  template: '<h2>This is Home page</h2>'
+  template: `<center>
+              <h1>Pi Floor</h1>
+              <br />
+              <input type="file" />
+              <br /><br />
+              <button>Edit Questions</button>
+              <br /><br />
+              <button>Play Game</button>
+            </center>`
 })
