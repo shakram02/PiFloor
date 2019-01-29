77842fd6d8122dd5da3e875a81a31ae63c2f8d56
AhmedHammad <hammad97official@gmail.com>
2019-01-30 01:11:01 +0200

    Add dynamic slots to modal button title, Add onClose functionality

--- app/src/main/assets/modal.js
+++ app/src/main/assets/modal.js
@@ -6,17 +6,22 @@ var modalQues = Vue.component('modal', {
     },
     methods: {
         onClose: function() {
+            if(this.$parent.$options._componentTag === "uploader"){
+              if(localStorage.getItem("questions")){
+                //Store localStorage.questions to quetsion array
+              }
+            }
             this.$emit('close');
         }
     },
     template: `
     <div>
-        <button @click="showModal = true">Questions</button>
+        <button @click="showModal = true"><slot name="title"></slot></button>
         <transition name="modal" v-if="showModal">
             <div class="modal-mask">
             <div class="modal-wrapper">
                 <div class="modal-container">
-                <slot></slot>
+                <slot name="content"></slot>
                 <div class="modal-footer">
                     <slot name="footer">
                     <button class="modal-default-button" @click="showModal = false; onClose()">
@@ -31,4 +36,3 @@ var modalQues = Vue.component('modal', {
     </div>
   `
 })
-  
