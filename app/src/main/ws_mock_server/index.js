c74a94dba32eef13fb4878f924dac7babb647983
shakram02 <ahmedhamdyau@gmail.com>
2019-06-25 18:46:18 +0200

    print more details, show small guide

--- app/src/main/ws_mock_server/index.js
+++ app/src/main/ws_mock_server/index.js
@@ -3,7 +3,11 @@ const readline = require('readline');
 const PORT_NUMBER = 5444;
 const TOPIC_NAME = 'game';
 
-var server = new ws.Server({ port: PORT_NUMBER, path: '/' + TOPIC_NAME });
+/**
+ * TODO: Don't forget to adjust the server URL in the vue code
+ */
+
+ var server = new ws.Server({ port: PORT_NUMBER, path: '/' + TOPIC_NAME });
 var clients = [];
 function handleMessage(message) {
     console.log(message);
@@ -22,13 +26,18 @@ function sendWithTimeout(commaSeparatedData) {
 }
 
 server.on('listening', () => {
-    console.log("Listening...");
+    var address = server.address();
+    var ip = address.address;
+    var port = address.port;
+    console.log("Listening... [%s:%s]\nTo send a message type something like the following:1,1,1,2,3,1", ip, port);
 });
+
 server.on('connection', (socket, request) => {
-    console.log("Yayr!");
+    console.log("CONNECTION:%s", request.connection.remoteAddress);
     socket.on('message', handleMessage);
     clients.push(socket);
 });
+
 var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
