ce630f0c496d813b9db41ec1f4bc741f478dd6ca
AhmedHammad <hammad97official@gmail.com>
2019-06-25 19:17:27 +0200

    Add animation upon question answer

--- app/src/main/web/src/components/GameComponents/QuestionBody.vue
+++ app/src/main/web/src/components/GameComponents/QuestionBody.vue
@@ -1,6 +1,6 @@
 <template>
   <div >
-    <h1 v-bind:class="['shape-' + theme]" ref="question"><slot></slot></h1>
+    <h1 v-bind:class="['shape-' + theme]"><span ref="question"><slot></slot></span></h1>
   </div>
 </template>
 
@@ -39,8 +39,55 @@ h1{
 }
 .correct{
   color: lawngreen;
+  animation: shakeV 0.8s cubic-bezier(.36,.07,.19,.97) both;
 }
 .wrong{
   color: red;
+  animation: shakeH 0.8s cubic-bezier(.36,.07,.19,.97) both;
+  transform: translate3d(0, 0, 0);
+  backface-visibility: hidden;
+  perspective: 1000px;
+}
+
+.correct, .wrong{
+  transform: translate3d(0, 0, 0);
+  backface-visibility: hidden;
+  perspective: 1000px;
+}
+
+@keyframes shakeV {
+  10%, 90% {
+    transform: translate3d(0, -1px, 0);
+  }
+
+  20%, 80% {
+    transform: translate3d(0, 2px, 0);
+  }
+
+  30%, 50%, 70% {
+    transform: translate3d(0, -4px, 0);
+  }
+
+  40%, 60% {
+    transform: translate3d(0, 4px, 0);
+  }
+}
+
+@keyframes shakeH {
+  10%, 90% {
+    transform: translate3d(-1px, 0, 0);
+  }
+
+  20%, 80% {
+    transform: translate3d(2px, 0, 0);
+  }
+
+  30%, 50%, 70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+
+  40%, 60% {
+    transform: translate3d(4px, 0, 0);
+  }
 }
 </style>
